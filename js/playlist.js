9983dbab09632e4e6aecf873938820aa36d8ff92
lucky-victory <blizwonder25@gmail.com>
2021-09-09 22:51:43 +0100

    updated playlist.js

--- js/playlist.js
+++ js/playlist.js
@@ -1,13 +1,76 @@
 class Player {
   constructor(container, songData) {
-    this.container = container;
+    this.container = document.querySelector(container);
     this.player = new Audio();
     this.songData = songData;
     this.songs = [];
-    this.container.innerHTML=`
     
+    this.container.innerHTML=`
+            <div class="player">
+          <div class="player__image-box">
+            <img src="./images/pexels-dellon-thomas-3394347.jpg" alt="" class="player__image">
+          </div>
+          <div class="player__title">Give it all</div>
+          <div class="player__artist">john mario</div>
+          
+          <div class="player__progress-container">
+          <time class="player__time-elasped">--:--</time>
+            
+            <div class="player__progress">
+              <div class="player__progress-fill"></div>
+            <div class="player__buffer-progress"></div>
+            </div>
+          <time class="player__duration">--:--</time>
+          </div>
+          <div class="player__controls-container">
+            <button class="player__control-btn fi-rr-rewind" id="prev_btn"></button>
+            <button class="player__control-btn flaticon-play-button" id="play_pause_btn"></button>
+            <button class="player__control-btn fi-rr-forward" id="next_btn"></button>
+          </div>
+        </div>
+        <div class="song-list-container">
+          <div class="song-list-header"><span>up next</span></div>
+        <ul class="song-list">
 
-`
+        </ul>
+        </div>
+`;
+this.playerContainer=this.container.getElementById('player');
+this.playlistContainer=this.container.querySelector('.song-list');
+this.playerImage=this.container.querySelector('.player__image');
+this.playerTitle=this.container.querySelector('.player__title');
+this.playerArtist=this.container.querySelector('.player__artist');
+this.playerProgress=this.container.querySelector('.player__progress');
+this.playerProgressFill=this.container.querySelector('.player__progress-fill');
+this.playerBufferProgress=this.container.querySelector('.player__buffer-progress');
+this.playerDuration=this.container.querySelector('.player__duration');
+this.playerTimeElasped=this.container.querySelector('.player__time-elasped');
+
+// controls 
+this.playPauseBtn=this.container.getElementById('play_pause_btn');
+this.prevBtn=this.container.getElementById('next_btn');
+this.nextBtn=this.container.getElementById('prev_btn');
+
+
+// event listeners
+this.playPauseBtn.addEventListener('click',()=>{
+  this.playPauseSong();
+});
+this.prevBtn.addEventListener('click',()=>{
+  this.prevSong();
+});
+this.nextBtn.addEventListener('click',()=>{
+  this.nextSong();
+});
+this.player.addEventListener('timeupdate',()=>{
+  this.onTimeUpdate();
+});
+this.player.addEventListener('loadedmetadata',()=>{
+  this.onMetaDataLoad();
+});
+this.player.addEventListener('progress',()=>{
+  this.onProgress();
+});
   }
 
   playPauseSong() {
@@ -57,6 +120,27 @@ class Player {
     this._updatePlaylist(this.songs);
     this._setActiveSong(this.songs[this.songIndex]);
   }
+  onMetaDataLoad(){
+    const {duration} = this.player;
+    const MINUTES = Math.floor(duration/60);
+    let seconds = Math.floor(duration%60);
+    seconds < 10 ? '0'+seconds : seconds;
+this.playerDuration.textContent=`${MINUTES}:${seconds}`;
+}
+onTimeUpdate(){
+      const {currentTime,duration} = this.player;
+    const MINUTES = Math.floor(currentTime/60);
+    let seconds = Math.floor(currentTime%60);
+    seconds < 10 ? '0'+seconds : seconds;
+this.playerTimeElasped.textContent=`${MINUTES}:${seconds}`;
+this.playerProgressFill.style.width=(100/duration * currentTime)+'%';
+}
+seekPlayer(){
+  const {currentTime}=this.player
+}
+onProgress(){
+
+}
   _setActiveSong(song = {}) {
     this.playerImage = song.cover;
     this.playerTitle = song.title;
@@ -104,3 +188,4 @@ class Player {
   </li>`)
   }
 }
+new Player('#playlist_container',[{}])
